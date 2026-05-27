b9c8afcf34b2b25b9f891e785ea2c6320694bf4b
Balaji Kumar <balaji-001-gif@users.noreply.github.com>
2026-05-27 14:54:35 +0530

    ERPNext v15.109.0 - Full source code
    Official ERPNext v15.109.0 built on Frappe Framework v15. Includes Accounting, CRM, Stock, Manufacturing, Projects, Support, Assets, Buying, Selling, Subcontracting, Regional, EDI and many other modules.

---
+++ erpnext/buying/doctype/purchase_order/purchase_order_dashboard.py
@@ -0,0 +1,35 @@
+from frappe import _
+
+
+def get_data():
+	return {
+		"fieldname": "purchase_order",
+		"non_standard_fieldnames": {
+			"Journal Entry": "reference_name",
+			"Payment Entry": "reference_name",
+			"Payment Request": "reference_name",
+			"Auto Repeat": "reference_document",
+		},
+		"internal_links": {
+			"Material Request": ["items", "material_request"],
+			"Supplier Quotation": ["items", "supplier_quotation"],
+			"Project": ["items", "project"],
+			"Sales Order": ["items", "sales_order"],
+		},
+		"transactions": [
+			{"label": _("Related"), "items": ["Purchase Receipt", "Purchase Invoice", "Sales Order"]},
+			{"label": _("Payment"), "items": ["Payment Entry", "Journal Entry", "Payment Request"]},
+			{
+				"label": _("Reference"),
+				"items": ["Supplier Quotation", "Project", "Auto Repeat"],
+			},
+			{
+				"label": _("Manufacturing"),
+				"items": ["Material Request"],
+			},
+			{
+				"label": _("Sub-contracting"),
+				"items": ["Subcontracting Order", "Subcontracting Receipt", "Stock Entry"],
+			},
+		],
+	}
