0e1233c10b96a97da6c27c2b049c7cbc0a67b664
Balaji Kumar <balaji-001-gif@users.noreply.github.com>
2026-06-01 12:10:35 +0530

    fix: SyntaxError in purchase_receipt.py - frappe.throw and def on same line

--- erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
+++ erpnext/stock/doctype/purchase_receipt/purchase_receipt.py
@@ -333,7 +333,9 @@ class PurchaseReceipt(BuyingController):
 		):
 			for d in self.get("items"):
 				if not d.purchase_order:
-					frappe.throw(_("Purchase Order number required for Item {0}").format(d.item_code))	def get_already_received_qty(self, po, po_detail):
+					frappe.throw(_("Purchase Order number required for Item {0}").format(d.item_code))
+
+	def get_already_received_qty(self, po, po_detail):
 		qty = frappe.db.sql(
 			"""select sum(qty) from `tabPurchase Receipt Item`
 			where purchase_order_item = %s and docstatus = 1
